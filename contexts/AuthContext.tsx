43a3de85795857fe55dc86e297ef3705dee6f232
JOAO VITOR CAVALCANTE BARBOSA <117310@pmenos.com.br>
2022-03-31 18:53:45 -0300

    feat: recuperando estado da autenticação

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import Router from "next/router";
-import { setCookie } from "nookies";
-import { createContext, ReactElement, useState } from "react";
+import { parseCookies, setCookie } from "nookies";
+import { createContext, ReactElement, useEffect, useState } from "react";
 import { api } from "../services/api";
 
 type User = {
@@ -24,15 +24,38 @@ type AuthProviderProps = {
   children: ReactElement;
 };
 
+type SessionReponse = {
+  token: string;
+  refreshToken: string;
+  roles: string[];
+  permissions: string[];
+}
+
 export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { 'nextauth.token': token } = parseCookies();
+
+    if (token) {
+      api.get('/me').then(response => {
+        const { email, permissions, roles } = response.data;
+
+        setUser({
+          email,
+          permissions,
+          roles
+        });
+      });
+    }
+  }, []);
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
-      const response = await api.post<User>("/sessions", {
+      const response = await api.post<SessionReponse>("/sessions", {
         email,
         password,
       });
@@ -44,7 +67,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
         path: "/",
       });
 
-      setCookie(undefined, "nexthauth.refreshToken", refreshToken, , {
+      setCookie(undefined, "nexthauth.refreshToken", refreshToken, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
         path: "/",
       });
@@ -54,6 +77,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
         permissions,
         roles,
       });
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
       Router.push("/dashboard");
     } catch (err) {
